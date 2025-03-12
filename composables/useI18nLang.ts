597b56d5c61d5426eb60b3a1186e214a2280eac3
JK <jacky8595125@gmail.com>
2025-03-12 17:10:59 +0800

    fix - repair i18n error by usei18nLang

--- composables/useI18nLang.ts
+++ composables/useI18nLang.ts
@@ -1,43 +1,53 @@
 export const useI18nLang = () => {
   const { t, setLocale } = useI18n()
 
-  const objLangDemo = reactive({
-    welcome: t('welcome'),
-    greeting: (params = '') => t('greeting', { name: params }),
+  const objLangDemo = computed(() => {
+    return {
+      welcome: t('welcome'),
+      greeting: (params = '') => t('greeting', { name: params }),
+    }
   })
 
-  const objLangCommon = reactive({
-    update_data: t('update_data'),
-    true: t('true'),
-    false: t('false'),
+  const objLangCommon = computed(() => {
+    return {
+      update_data: t('update_data'),
+      true: t('true'),
+      false: t('false'),
+    }
   })
 
-  const objLangUserLogin = reactive({
-    login: t('login'),
-    login_account_placeholder: t('login_account_placeholder'),
-    login_password_placeholder: t('login_password_placeholder'),
-    login_password_can_show_password_tooltip: t(
-      'login_password_can_show_password_tooltip'
-    ),
-    login_remember_me: t('login_remember_me'),
+  const objLangUserLogin = computed(() => {
+    return {
+      login: t('login'),
+      login_account_placeholder: t('login_account_placeholder'),
+      login_password_placeholder: t('login_password_placeholder'),
+      login_password_can_show_password_tooltip: t(
+        'login_password_can_show_password_tooltip'
+      ),
+      login_remember_me: t('login_remember_me'),
+    }
   })
 
-  const objLangProfile = reactive({
-    profile_setting: t('profile_setting'),
-    profile_msg: t('profile_msg'),
-    profile_preference_setting: t('profile_preference_setting'),
-    profile_security_log: t('profile_security_log'),
-    logout: t('logout'),
+  const objLangProfile = computed(() => {
+    return {
+      profile_setting: t('profile_setting'),
+      profile_msg: t('profile_msg'),
+      profile_preference_setting: t('profile_preference_setting'),
+      profile_security_log: t('profile_security_log'),
+      logout: t('logout'),
+    }
   })
 
-  const objLangPagination = reactive({
-    pagination_show_total: (total: string | number) =>
-      t('pagination_show_total', { total }),
-    pagination_items_per_page: t('pagination_items_per_page'),
-    pagination_jump_to: t('pagination_jump_to'),
-    pagination_page: t('pagination_page'),
-    pagination_prev_page: t('pagination_prev_page'),
-    pagination_next_page: t('pagination_next_page'),
+  const objLangPagination = computed(() => {
+    return {
+      pagination_show_total: (total: string | number) =>
+        t('pagination_show_total', { total }),
+      pagination_items_per_page: t('pagination_items_per_page'),
+      pagination_jump_to: t('pagination_jump_to'),
+      pagination_page: t('pagination_page'),
+      pagination_prev_page: t('pagination_prev_page'),
+      pagination_next_page: t('pagination_next_page'),
+    }
   })
 
   const setLocaleToUS = () => setLocale('en-US')
